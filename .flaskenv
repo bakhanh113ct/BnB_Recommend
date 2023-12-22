ab63d72e4b9cf82e0c089c4946bfff278ba53275
bakhanh113ct <84719735+bakhanh113ct@users.noreply.github.com>
2023-12-23 01:45:59 +0700

    first commit

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+export FLASK_ENV=development
+export SQLALCHEMY_DB_URI=postgresql://postgres:1@localhost:5432/BnB_D
---
+++ database.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import string
+import random
+import json
+db = SQLAlchemy()
+
+
+class Product(db.Model):
+    __tablename__ = 'Products'
+    Id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(45), nullable=False)
+    Desc = db.Column(db.String(225), nullable=False)
+    Price = db.Column(db.Integer, nullable=False)
+    BrandId = db.Column(db.Integer, nullable=False)
+    ImportPrice = db.Column(db.Integer, nullable=False)
+    CategoryId = db.Column(db.Integer, nullable=False)
+    Sold = db.Column(db.Integer, nullable=False)
+    Images = db.Column(db.Text, nullable=False)
+    
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __repr__(self) -> str:
+        return 'Product>>> {self.id}'
+
